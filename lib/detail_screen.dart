58000ad366efaf00fe3ce0dd1451b993e554f4b6
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-01-24 20:08:39 +0700

    Update screen in detail_screen.dart

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -5,63 +5,97 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              child: const Text(
-                "Farm House",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Image.asset("images/farm-house.jpg"),
+              Container(
+                child: const Text(
+                  "Farm House",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Column(
-                    children: const [
-                      Icon(Icons.calendar_today),
-                      SizedBox(
-                        height: 8.0,
-                      ),
-                      Text("Open Every Day"),
-                    ],
-                  ),
-                  Column(
-                    children: const [
-                      Icon(Icons.timer_outlined),
-                      SizedBox(
-                        height: 8.0,
-                      ),
-                      Text("09:00 - 12:00"),
-                    ],
-                  ),
-                  Column(
-                    children: const [
-                      Icon(Icons.monetization_on),
-                      SizedBox(
-                        height: 8.0,
-                      ),
-                      Text("Rp 25.000"),
-                    ],
-                  ),
-                ],
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Column(
+                      children: const [
+                        Icon(Icons.calendar_today),
+                        SizedBox(
+                          height: 8.0,
+                        ),
+                        Text("Open Every Day"),
+                      ],
+                    ),
+                    Column(
+                      children: const [
+                        Icon(Icons.timer_outlined),
+                        SizedBox(
+                          height: 8.0,
+                        ),
+                        Text("09:00 - 12:00"),
+                      ],
+                    ),
+                    Column(
+                      children: const [
+                        Icon(Icons.monetization_on),
+                        SizedBox(
+                          height: 8.0,
+                        ),
+                        Text("Rp 25.000"),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                "Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16.0),
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                child: const Text(
+                  "Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16.0),
+                ),
               ),
-            ),
-          ],
+              Container(
+                height: 150,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10.0),
+                          child: Image.network(
+                              "https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg")),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10.0),
+                        child: Image.network(
+                            'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10.0),
+                        child: Image.network(
+                            'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
