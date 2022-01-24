4691030fb5f9f858724d395ef7074a8d4e8ee245
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-01-24 11:45:14 +0700

    Body content detail screen

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -18,8 +18,49 @@ class DetailScreen extends StatelessWidget {
                 ),
               ),
             ),
-            Container(),
-            Container(),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: const [
+                      Icon(Icons.calendar_today),
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      Text("Open Every Day"),
+                    ],
+                  ),
+                  Column(
+                    children: const [
+                      Icon(Icons.timer_outlined),
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      Text("09:00 - 12:00"),
+                    ],
+                  ),
+                  Column(
+                    children: const [
+                      Icon(Icons.monetization_on),
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      Text("Rp 25.000"),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                "Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ),
           ],
         ),
       ),
