aa932ffb3f5d9da7e3b9138459d700e93d6edacb
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-01-24 20:23:26 +0700

    Navigation to detail screen

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.amber
       ),
-      home: DetailScreen(),
+      home: MainScreen(),
     );
   }
 }
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,15 +1,49 @@
+import 'package:daftar_wisata/detail_screen.dart';
 import 'package:flutter/material.dart';
 
-class MainScreen extends StatelessWidget{
+class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Wisata Bandung'),
       ),
-      body: Card(
-        child: Row(
-
+      body: InkWell(
+        onTap: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context){
+            return DetailScreen();
+          }));
+        },
+        child: Card(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                flex: 1,
+                child: Image.asset("images/farm-house.jpg"),
+              ),
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "Farm House",
+                        style: TextStyle(fontSize: 16.0),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Text("Lembang"),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
